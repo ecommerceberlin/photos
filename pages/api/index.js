d131ddcdc72ca78e7ce6dd3f1d6c6e1c9fb7110f
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-29 00:45:13 +0200

    redirect to cloudinary

--- pages/api/index.js
+++ pages/api/index.js
@@ -30,15 +30,15 @@ async function handler(req, res) {
   const { id, folder } = req.query
 
   if(!id || !folder){
-    res.send("bad params")
+    res.status(400).send("bad params")
   }
     
-  const query = await cloudinary.utils.download_folder(`${folder}/${id}`, {
+  const url = await cloudinary.utils.download_folder(`${folder}/${id}`, {
       target_public_id: `${folder}_${id}`,
       flatten_folders: true
   });
   
-  res.status(200).json({zip: query})
+  res.redirect(307, url)
 
 }
 
