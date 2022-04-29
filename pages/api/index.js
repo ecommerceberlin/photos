8fd5c875443f3bac9c5f1acfb1bb64c0e48687f6
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-29 11:27:27 +0200

    fixed headers already sent by

--- pages/api/index.js
+++ pages/api/index.js
@@ -30,16 +30,19 @@ async function handler(req, res) {
   const { id, folder } = req.query
 
   if(!id || !folder){
+  
     res.status(400).send("bad params")
-  }
-    
-  const url = await cloudinary.utils.download_folder(`${folder}/${id}`, {
+  
+  }else{
+
+    const url = await cloudinary.utils.download_folder(`${folder}/${id}`, {
       target_public_id: `${folder}_${id}`,
       flatten_folders: true
-  });
+    });
   
-  res.redirect(307, url)
-
+    res.redirect(307, url)
+  }
+    
 }
 
 export default handler
