2aa7d3cb40232eb0f862d4af11d015c87e7d98cd
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-13 10:47:26 +0200

    ebe photos

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+
+  async rewrites() {
+    return [
+      {
+        source: '/:folder/:id',
+        destination: '/api/?folder=:folder&id=:id',
+      },
+    ]
+  },
+
+
 }
 
 module.exports = nextConfig
